c90a8e5b2a537187aeb4e70dcab1fd0e2f00012d
Martini <martini@et.esiea.fr>
2015-12-11 12:56:23 +0100

    Initial commit

---
+++ BlogBundle/Controller/AnnonceController.php
@@ -0,0 +1,48 @@
+<?php
+
+// src/Alex/BlogBundle/Controller/AnnonceController.php
+
+namespace Alex\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AnnonceController extends Controller
+{
+	
+  // La route fait appel à AlexBlogBundle:Advert:view,
+  // on doit donc définir la méthode viewAction.
+  // On donne à cette méthode l'argument $id, pour
+  // correspondre au paramètre {id} de la route
+  public function viewAction($id)
+  {
+    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+
+    // Ici, on récupèrera depuis la base de données
+    // l'annonce correspondant à l'id $id.
+    // Puis on passera l'annonce à la vue pour
+    // qu'elle puisse l'afficher
+
+    return new Response("Affichage de l'annonce d'id : ".$id);
+  }
+  
+  // On récupère tous les paramètres en arguments de la méthode
+    public function viewSlugAction($slug, $year, $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
+    }
+  public function indexAction()
+  {
+    // On veut avoir l'URL de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'alex_blog_view', // 1er argument : le nom de la route
+            array('id' => 5)    // 2e argument : les valeurs des paramètres
+        );
+        // $url vaut « /platform/advert/5 »
+
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+  }
+}
