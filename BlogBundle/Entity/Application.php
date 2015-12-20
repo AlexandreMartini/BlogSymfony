e142ee5ba357d5fa57dc324c5178250ba2d7dc53
Martini <martini@et.esiea.fr>
2015-12-20 14:18:34 +0100

    Ajout de l'entité Application

---
+++ BlogBundle/Entity/Application.php
@@ -0,0 +1,110 @@
+<?php
+// src/OC/PlatformBundle/Entity/Application.php
+
+namespace Alex\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="Alex\BlogBundle\Entity\ApplicationRepository")
+ */
+class Application
+{
+  /**
+   * @ORM\ManyToOne(targetEntity="Alex\BlogBundle\Entity\Annonce")
+   * @ORM\JoinColumn(nullable=false)
+   */
+  private $advert;
+
+  /**
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+
+  /**
+   * @ORM\Column(name="author", type="string", length=255)
+   */
+  private $author;
+
+  /**
+   * @ORM\Column(name="content", type="text")
+   */
+  private $content;
+
+  /**
+   * @ORM\Column(name="date", type="datetime")
+   */
+  private $date;
+  
+  public function __construct()
+  {
+    $this->date = new \Datetime();
+  }
+
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function setAuthor($author)
+  {
+    $this->author = $author;
+
+    return $this;
+  }
+
+  public function getAuthor()
+  {
+    return $this->author;
+  }
+
+  public function setContent($content)
+  {
+    $this->content = $content;
+
+    return $this;
+  }
+
+  public function getContent()
+  {
+    return $this->content;
+  }
+
+  public function setDate($date)
+  {
+    $this->date = $date;
+
+    return $this;
+  }
+
+  public function getDate()
+  {
+    return $this->date;
+  }
+
+    /**
+     * Set advert
+     *
+     * @param \Alex\BlogBundle\Entity\Annonce $advert
+     *
+     * @return Application
+     */
+    public function setAdvert(\Alex\BlogBundle\Entity\Annonce $advert)
+    {
+        $this->advert = $advert;
+
+        return $this;
+    }
+
+    /**
+     * Get advert
+     *
+     * @return \Alex\BlogBundle\Entity\Annonce
+     */
+    public function getAdvert()
+    {
+        return $this->advert;
+    }
+}
